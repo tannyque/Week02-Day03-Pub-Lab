a9d5becfbef15c031dfd6730370e3b81a0790a42
tannyque <tannyque1992@gmail.com>
2018-07-29 18:09:48 +0100

    Adds indentation

--- drink.rb
+++ drink.rb
@@ -3,9 +3,9 @@ class Drink
   attr_reader :name, :price, :alcohol_level
 
   def initialize(name, price, alcohol_level)
-  @name = name
-  @price = price
-  @alcohol_level = alcohol_level
+    @name = name
+    @price = price
+    @alcohol_level = alcohol_level
   end
 
 end
